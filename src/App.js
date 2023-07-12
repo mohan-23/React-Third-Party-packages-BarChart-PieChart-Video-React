028d1f4a4ce766c75c72038c1b38f32d867f227a
mohan-23 <mohankandukoori23@gmail.com>
2023-07-12 12:17:12 +0000

    React Third-Party Packages BarChart PieChart Video

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import VideoPlayer from './components/VideoPlayer'
+import ChartView from './components/ChartView'
+import PieChartView from './components/PieChartView'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <VideoPlayer />
+    <ChartView />
+    <PieChartView />
+  </>
+)
+
+export default App
